55b04614121596699179ac5702836c7807cb92c2
Will Sutherland <wgpsutherland@gmail.com>
2014-06-08 16:20:49 +0100

    seeing if it works for the 1975

--- js.js
+++ js.js
@@ -4,7 +4,7 @@
 								    	type: "GET",
 								        dataType: "jsonp",
 								        cache: false,
-								        url: "https://api.instagram.com/v1/users/USERID/media/recent/?access_token=222716143.cde9b68.04e77477b2fd4524a0d2a91fb170142f",
+								        url: "https://api.instagram.com/v1/users/43153969/media/recent/?access_token=222716143.cde9b68.04e77477b2fd4524a0d2a91fb170142f",
 								        success: function(data) {
 									        for (var i = 0; i < 4; i++) {
 									        	$("#instafeed").append("<div class='instaframe'>
