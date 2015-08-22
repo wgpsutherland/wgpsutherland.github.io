7ba3c417357b7a5611cf3952b42dfcb9c9a685c0
Will Sutherland <wgpsutherland@gmail.com>
2015-08-22 01:01:25 +0100

    Type text for main page navigation
    The code is very messy, it will be refactored

--- public/js/views/sections/HomeTitle.js
+++ public/js/views/sections/HomeTitle.js
@@ -22,21 +22,70 @@ define([
 
         // create the typing animation
         typeText: function () {
-            var self = this;
-            var speed = 100;
-            var wordList = 'Will Sutherland'.split('');
-            _.each(wordList, function (letter, i) {
+
+            const self = this;
+
+            const nameTypingCaret = $("<span>").text("|").addClass("name-type-caret");
+            const linksTypingCaret = $("<span>").text("|").addClass("links-type-caret");
+
+            self.$(".name").append(nameTypingCaret);
+
+            const speed = 100;
+
+            var time = 0;
+
+            var name = 'Will Sutherland';
+            var homeLinks = ['about', 'projects'];
+            console.log(homeLinks);
+
+            _.each(name.split(''), function (letter, i) {
                 setTimeout(function () {
-                    self.appendLetter((i === 0) ? '.type-caret' : '.name span:last-child', letter);
-                },(i + 1) * speed);
+                    self.appendLetter((i === 0) ? '.name-type-caret' : '.name span:last-child', letter);
+                }, time += speed);
             });
             setTimeout(function () {
-                self.$('.type-caret').toggleClass('blinker');
-            }, (wordList.length * speed) + 400);
+                // don't just delete it as the words will jump to fill the void
+                self.$('.name-type-caret').toggleClass('clear');
+                self.$(".home-links").append(linksTypingCaret);
+
+                //self.$('.type-caret').toggleClass('blinker'); // save this for the end
+            }, time += speed);
+            _.each(homeLinks, function (link, i) {
+                var list = link.split('');
+                self.$(".home-links").append("<li class=li-" + link + "><a class=link-" + link + " href='#/" + link + "'></a></li>");
+
+                _.each(list, function (letter) {
+                    setTimeout(function () {
+                        self.$(".link-" + link).append($("<span>").html(letter));
+                    }, time += speed);
+                });
+
+                if (i !== homeLinks.length - 1) {
+                    var slashLi = $("<li>");
+                    setTimeout(function () {
+                        self.$(".li-" + link).append(slashLi);
+                    }, time);
+                    setTimeout(function () {
+                        slashLi.append(($("<span>").html("&nbsp;")));
+                    }, time += speed);
+                    setTimeout(function () {
+                        slashLi.append(($("<span>").html("/")));
+                    }, time += speed);
+                    setTimeout(function () {
+                        slashLi.append(($("<span>").html("/")));
+                    }, time += speed);
+                    setTimeout(function () {
+                        slashLi.append(($("<span>").html("&nbsp;")));
+                    }, time += speed);
+                }
+            });
+            setTimeout(function () {
+                self.$('.links-type-caret').toggleClass('blinker');
+            }, time += (speed * 4));
         },
 
         appendLetter: function (className, letter) {
-            this.$(className).before("<span>" + letter + "</span>");
+            this.$(className).before($("<span>").html(letter));
         }
     });
 });
